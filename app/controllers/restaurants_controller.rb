8cee32535ca7ed84db474ace28fd484bb1154eb3
Martyn McGowan <martyn_mcgowan@yahoo.com>
2022-02-16 15:13:55 -0500

    added styling and fixed pathing issues

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,16 +6,35 @@ class RestaurantsController < ApplicationController
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
-  def show; end
+  def show
+    @title =
+      if @restaurant.reviews.empty?
+        'No reviews yet!'
+      else
+        "All #{@restaurant.name} reviews:"
+      end
+    @review = Review.new
+  end
 
   private
 
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
